1710dff530f454e1963abf621e19ed99ae88fb6c
Loren Cheung <lccheung@gmail.com>
2011-05-22 23:21:32 -0700

    implementing rank score to suggest the best word matched

--- words.rb
+++ words.rb
@@ -7,8 +7,8 @@ class String
     lastletter = ""
     puts word if log
     # base case, return the obvious
-    return true if word.downcase == self.downcase
-
+    return 4*self.size if word.downcase == self.downcase
+    score = 0
     # loop through each letter of word and match against corresponding letter in dictionary word
     # stop when either word reaches the end
     while (i < selfarr.size and j < wordarr.size)
@@ -16,15 +16,18 @@ class String
       if selfarr[i].downcase == wordarr[j].downcase
         # OK, it's a match
         puts "  matched letter" if log
+        score += 4
       else 
         # NOT A MATCH, but check exceptions
         if (i > 0 and selfarr[i].downcase == selfarr[i-1].downcase)
           #if selfarr[i].match(/^#{selfarr[i-1]}$/i) 
           # is the previous letter repeating?
           puts "  repeating consonant" if log
+          score += 2
           j -= 1
         elsif selfarr[i].match(/[aeiou]/i) and wordarr[j].match(/[aeiou]/i)
           # is it a vowel?
+          score += 1
           puts "  diff vowel" if log
         else
           puts " NOT MATCH" if log
@@ -39,7 +42,11 @@ class String
     if i == selfarr.size 
       # TODO if end of self, return true only if end of dictionary word
       #puts "reached end of self: #{self}(#{self.size},#{i}), dict: #{word}(#{word.size})(#{j}) may be greedy"
-      return j == wordarr.size
+      if j == wordarr.size
+        return score
+      else
+        return false
+      end 
     else 
       #puts "reached end of dict: #{word}(#{word.size},#{j}), self: #{self}(#{self.size})(#{i}) may be greedy"
       while (i < selfarr.size)
@@ -48,7 +55,7 @@ class String
         end 
         i += 1
       end
-      return true
+      return score
     end 
   end 
 end 
